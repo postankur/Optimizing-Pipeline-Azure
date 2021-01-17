9547a21bea86c73dc3aacb4a51d196775acc457e
postankur <postankur@gmail.com>
2021-01-17 18:03:23 -0500

    Add files via upload

--- train.py
+++ train.py
@@ -9,48 +9,32 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Workspace, Dataset
+import clean_data
+import joblib
+from sklearn.linear_model import Ridge
+
+from azureml.core import Model
+from azureml.core.resource_configuration import ResourceConfiguration
 
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+data_url_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = TabularDatasetFactory.from_delimited_files(path=data_url_path)
+df = ds.to_pandas_dataframe()
 
-x, y = clean_data(ds)
+# ds = ### YOUR CODE HERE ###
+x, y = clean_data.clean_data(ds)
 
 # TODO: Split data into train and test sets.
-
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=50)
+    
 ### YOUR CODE HERE ###a
 
 run = Run.get_context()
-
-def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
-    # Clean and one hot encode data
-    x_df = data.to_pandas_dataframe().dropna()
-    jobs = pd.get_dummies(x_df.job, prefix="job")
-    x_df.drop("job", inplace=True, axis=1)
-    x_df = x_df.join(jobs)
-    x_df["marital"] = x_df.marital.apply(lambda s: 1 if s == "married" else 0)
-    x_df["default"] = x_df.default.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["housing"] = x_df.housing.apply(lambda s: 1 if s == "yes" else 0)
-    x_df["loan"] = x_df.loan.apply(lambda s: 1 if s == "yes" else 0)
-    contact = pd.get_dummies(x_df.contact, prefix="contact")
-    x_df.drop("contact", inplace=True, axis=1)
-    x_df = x_df.join(contact)
-    education = pd.get_dummies(x_df.education, prefix="education")
-    x_df.drop("education", inplace=True, axis=1)
-    x_df = x_df.join(education)
-    x_df["month"] = x_df.month.map(months)
-    x_df["day_of_week"] = x_df.day_of_week.map(weekdays)
-    x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
-
-    y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
-
+ 
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
@@ -67,6 +51,21 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('./outputs', exist_ok=True)  
+     
+    model_file_name = 'amptrain_1.pkl'
+    # save model in the outputs folder so it automatically get uploaded
+    with open(model_file_name, "wb") as file:
+        joblib.dump(value=model, filename=os.path.join('./outputs/',
+                                                        model_file_name))
+                                                     
+    # joblib.dump(model, 'model.joblib')
+    # model1 = run.register_model(model_name='sklearn1', 
+    #                       model_path='/model.joblib',
+    #                       model_framework=Model.Framework.SCIKITLEARN,
+    #                       model_framework_version='0.19.1',
+    #                       resource_configuration=ResourceConfiguration(cpu=1, memory_in_gb=0.5))
 
 if __name__ == '__main__':
     main()
